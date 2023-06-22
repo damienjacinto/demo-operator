84d6a5c66ca45bb124dc75cb162edc9bb1895508
damien-jacinto <damien.jacinto@finalcad.com>
2023-06-22 13:43:37 +0200

    add log

--- controllers/killoldpod_controller.go
+++ controllers/killoldpod_controller.go
@@ -69,9 +69,11 @@ func (r *KillOldPodReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	if err != nil {
 		return ctrl.Result{}, err
 	}
+	mylog.Info(podList)
 
 	// Filter the Pods that haven't been updated for the specified number of days
 	threshold := time.Now().AddDate(0, 0, -1*int(killOldPod.Spec.Minutes))
+	mylog.Info("Threshold", threshold)
 	oldPods := []corev1.Pod{}
 	for _, pod := range podList.Items {
 		if pod.CreationTimestamp.Time.Before(threshold) {
